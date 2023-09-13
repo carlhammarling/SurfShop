e092bd55a07d9b801b4b49ab6539a3658735d4d9
Carl Hammarling <carlhammarling@gmail.com>
2023-09-13 14:48:54 +0200

    Responsiv menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,29 +4,25 @@ import "../../assets/images/surf-logo.png";
 import { useData } from "../../Context/DataContext";
 
 const Navbar = ({ setShowCart }: HandleCartProps) => {
-  
   const { cart } = useData();
 
   return (
     <div className="Navbar">
       <div className="logo"></div>
       <ul className="Nav">
-        <li>
+        <li className="desktopLink">
           <NavLink to="/">HOME</NavLink>
         </li>
-        <li>
+        <li className="desktopLink">
           <NavLink to="surfproducts">SURFBOARDS</NavLink>
         </li>
-        <li>
+        <li className="desktopLink">
           <NavLink to="kiteproducts">KITESURF</NavLink>
         </li>
-        <li>
-          <NavLink to="wetsuits">WETSUITS</NavLink>
-        </li>
-        <li>
+        <li className="desktopLink">
           <NavLink to="swimwear">SWIMWEAR</NavLink>
         </li>
-        <li>
+        <li className="desktopLink">
           <NavLink className="redFont" to="admin">
             ADMIN
           </NavLink>
@@ -37,11 +33,16 @@ const Navbar = ({ setShowCart }: HandleCartProps) => {
             onClick={() => setShowCart((state) => !state)}
           >
             <i className="fa-solid fa-cart-shopping"></i>
-            
-              <div className={`cartCount ${cart && cart.length > 0 ? '' : 'hide'}`}>{cart.length}</div>
-            
 
+            <div
+              className={`cartCount ${cart && cart.length > 0 ? "" : "hide"}`}
+            >
+              {cart.length}
+            </div>
           </div>
+        </li>
+        <li className="dropDown">
+          <i className="fa-solid fa-bars"></i>
         </li>
       </ul>
     </div>
